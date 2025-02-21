c4da471011e14ab84e95cb890057900cac87f3e2
Marco-de-Sá <marcojb.desa@gmail.com>
2025-02-21 00:14:06 +0000

    restarting from the beginning

--- prisonerHats.py
+++ prisonerHats.py
@@ -1,13 +1,46 @@
 from random import randint
 
 def first_prisoner(prisoners):
-    print()
+    hatCount = 0
+    guesses = []
 
-def prisoner_guess():
-    print()
+    # counts the amount of white hats
+    for i in range(len(prisoners)-1):
+        if prisoners[i] == 1:
+            hatCount += 1
+    # guesses white if there are an even amount of white hats and black if there are an uneven amount of white hats
+    if hatCount % 2 == 0:
+        guesses.append("white")
+    if hatCount % 2 == 1:
+        guesses.append("black")
+    # parses the initial guesses
+    prisoner_guess(guesses, prisoners)
+
+def prisoner_guess(guesses, prisoners):
+    hatCount = 0
+    isEven = 0
+    if guesses[0] == "white":
+        isEven = 0
+    elif guesses[0] == "black":
+        isEven = 1
+
+    for i in range(2, len(prisoners) - 1):
+        currentPos = 2
+        for j in range(currentPos, len(prisoners) - 1):
+            if prisoners[j] == 1:
+                hatCount += 1
+        if hatCount%2 == isEven:
+            guesses.append("black")
+            isEven = 0
+        else:
+            guesses.append("white")
+            isEven = 1
+    print(guesses)
+
+
+
 
 # 0 = black 1 = white
 prisoners = [randint(0,1) for i in range(5)]
 print(prisoners)
-
 first_prisoner(prisoners)
