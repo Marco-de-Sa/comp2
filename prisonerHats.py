ff367dc4361f8afcaf1fe4de2883db117a00e61c
Marco-de-Sá <marcojb.desa@gmail.com>
2025-02-21 09:19:12 +0000

    restarting from the beginning

--- prisonerHats.py
+++ prisonerHats.py
@@ -20,24 +20,35 @@ def prisoner_guess(guesses, prisoners):
     hatCount = 0
     isEven = 0
     iterHat = 2
+    whiteOrBlack = "white"
+
+    # sets the isEven variable to 0 if white was guessed indicating an even amount of white hats and 0 if not
     if guesses[0] == "white":
         isEven = 0
     elif guesses[0] == "black":
         isEven = 1
-
-    for i in range(len(prisoners) - iterHat):
+    print(f"isEven: {isEven}")
+    for i in range(len(prisoners)):
         for j in range(len(prisoners) - iterHat):
             if prisoners[j] == 1:
-                hatcount += 1
-            iterHat += 1
-        print(hatcount)
-        hatcount = 0
+                hatCount += 1
+        iterHat += 1
+        print(hatCount)
         if hatCount%2 == isEven:
-            guesses.append("black")
-            isEven = 0
+            whiteOrBlack = "black"
+            guesses.append(whiteOrBlack)
+            if len(guesses) == len(prisoners):
+                break
         else:
-            guesses.append("white")
-            isEven = 1
+            whiteOrBlack = "white"
+            guesses.append(whiteOrBlack)
+            if isEven == 0:
+                isEven = 1
+            else:
+                isEven = 0
+            if len(guesses) == len(prisoners):
+                break
+        hatCount = 0
     print(guesses)
 
 
