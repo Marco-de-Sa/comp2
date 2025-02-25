ccc81413f8a64ed2f983b49bbddb1ad9c813474b
Marco-de-Sá <marcojb.desa@gmail.com>
2025-02-25 12:12:29 +0000

    added a oiwer of function

--- week2/recursion_exercise_1.py
+++ week2/recursion_exercise_1.py
@@ -1,7 +1,7 @@
 def count_down(num):
     if num == -1:
         print("boom")
-        exit()
+        return ""
     elif num > -1:
         print(num)
         count_down(num-1)
@@ -9,13 +9,23 @@ def count_down(num):
         print("num not positive")
 
 def sum_of(num):
-    somVan = num
-    if somVan == 0:
-        return 0
+    if num == 1:
+        return num
     else:
-        somVan += sum_of(num-1)
+        num += sum_of(num-1)
+        return num
+
+def power_of(a, b):
+    num = a
+    power = b
+    if power == 0:
+        return 1
+    num *= power_of(num, power-1)
+    return num
 
 n = int(input("input a number for the countdown"))
 print(sum_of(n))
 print()
 count_down(n)
+
+print(power_of(2, 3))
