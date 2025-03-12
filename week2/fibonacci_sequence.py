760ed0c68f4535f09508c8ec94cf5ba87a9398bd
Marco-de-Sá <marcojb.desa@gmail.com>
2025-03-12 10:30:11 +0000

    removed commented out code and added actual comments

--- week2/fibonacci_sequence.py
+++ week2/fibonacci_sequence.py
@@ -1,12 +1,19 @@
 def fib(n, s, sn):
+    # this if statement detects whether the function is at its end
     if sn == n-2:
         return s[2:]
+    # appends the sum of the previous two values to the last spot in the list
     s.append(s[sn] + s[sn+1])
+    # calls the function again and makes the result equal to the returned value
     s = fib(n, s, sn+1)
+    # returns the list s after all the recursions are done
     return s
-    # print(s[2:])
 
+# asks the user how long they would like the sequence to be
 numbers = int(input("till what n is the fib sequence"))
+# sets the starting index of the sequence
 sNum = 0
+# sets the initial list for the sequence
 start = [1, 1]
+# parses the variables into the fib function
 print(fib(numbers, start, sNum))
