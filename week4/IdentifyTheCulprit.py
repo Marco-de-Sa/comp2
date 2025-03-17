39cc82c3c7d74f07963d06261509169a5a7eacf5
Marco-de-Sá <marcojb.desa@gmail.com>
2025-03-17 10:48:41 +0000

    fixed virus counter

--- week4/IdentifyTheCulprit.py
+++ week4/IdentifyTheCulprit.py
@@ -1,12 +1,16 @@
 from random import randint
 
-server = [0, 0, 0, 0, 0, 0, 0]
-server[randint(0,6)] = 1
+def bin_search(ser, bin_sel, sel_num):
+    if 1 in ser[:sel_num] and not (len(sel_num) == 1 and (1 in ser[sel_num:] or 1 in ser[:sel_num])):
+        bin_search(ser, bin_sel, sel_num//2)
+    elif 1 in ser[sel_num:] and sel_num <= bin_sel//2 and not(len(ser(sel_num)) == 1 and (1 in ser[sel_num:] or 1 in ser[:sel_num])):
+        bin_search(ser, bin_sel, bin_sel//2 + sel_num//2)
+    else:
+        return
+
+server = [0, 0, 0, 0, 0, 0]
+server[randint(0,5)] = 1
 attempts = 0
-# planned: use binary search
 binarySection = len(server)//2
-if 1 in server[binarySection]:
-    attempts+=1
-else:
-    binarySection += binarySection//2
-    if 1 in server[binarySection]:
+
+bin_search(server, len(server), len(server)//2)
