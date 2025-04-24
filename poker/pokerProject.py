469fcc72d57e432fcc44c6ee3bf8e5b732f3c2df
Marco-de-Sá <marcojb.desa@gmail.com>
2025-04-24 12:23:37 +0100

    made the sort_deck method in the GameDeck class functional

--- poker/pokerProject.py
+++ poker/pokerProject.py
@@ -1,17 +1,22 @@
 from random import randint
 
+# todo add a class dedicated to handling the cards dealt to the player and by extension detecting what hands they have
+# todo add a way to count how many times specific hands have been encountered
+
 class GameDeck:
     def __init__(self):
         spCard = {1: "ace", 2: "jack", 3: "queen", 4: "king"}
         suit = {1: "clubs", 2: "spades", 3: "hearts", 4: "diamonds"}
-        deck = [f"{spCard[1]} {suit[i]}" for i in range(1, 5)]
+        deck = [f"{spCard[1]} of {suit[i]}" for i in range(1, 5)]
         for i in range(2, 11):
             for j in range(1, 5):
-                deck.append(f"{i} {suit[j]}")
+                deck.append(f"{i} of {suit[j]}")
         for i in range(2, 5):
             for j in range(1, 5):
-                deck.append(f"{spCard[i]} {suit[j]}")
+                deck.append(f"{spCard[i]} of {suit[j]}")
         self.deck = deck
+        self.spCard = spCard
+        self.suit = suit
 
     def count_suit(self, target):
         temp = 0
@@ -25,6 +30,7 @@ class GameDeck:
             self.deck.append(hand.pop())
 
     def deal_cards(self, card_count):
+        # todo add a way to sort cards before handing them out
         temp = []
         for i in range(int(card_count)):
             temp.append(self.deck.pop())
@@ -32,17 +38,21 @@ class GameDeck:
 
     def sort_deck(self):
         # wip do not use
+        # todo add a method to sort the cards by suit and number
         tempDeck = []
+        for i in range(len(self.deck)):
+            if self.deck[i].split(' ')[0] == "ace":
+                tempDeck.append(self.deck[i])
+
         for i in range(2, 11): # to sort card numbers
-            count = 0
-            for j in range(len(self.deck)-1):# to get iterate through the deck
+            for j in range(len(self.deck)):# to get iterate through the deck
                 if self.deck[j].split(' ')[0] == str(i):
-                    tempDeck.append(self.deck.pop(j))
-                    count+=1
-                    j-=1
-                if count == 4:
-                    count = 0
-                    continue
+                    tempDeck.append(self.deck[j])
+
+        for i in range(2, 5):
+            for j in range(len(self.deck)):
+                if self.deck[j].split(' ')[0] == self.spCard[i]:
+                    tempDeck.append(self.deck[j])
         self.deck = tempDeck
 
     def shuffle(self):
@@ -53,18 +63,18 @@ class GameDeck:
             self.deck[ind1] = self.deck[ind2]
             self.deck[ind2] = temp
 
-    def __str__(self):
+    def to_string(self):
         return f"{self.deck}"
 
 my_hand = GameDeck()
 my_hand.count_suit("hearts")
-print(my_hand.__str__())
+print(my_hand.to_string())
 my_hand.shuffle()
-print(my_hand.__str__())
+print(my_hand.to_string())
 tem = my_hand.deal_cards(4)
 print(tem)
-print(my_hand.__str__())
+print(my_hand.to_string())
 my_hand.rejoin(tem)
-print(my_hand.__str__())
+print(my_hand.to_string())
 my_hand.sort_deck()
-print(my_hand.__str__())
+print(my_hand.to_string())
