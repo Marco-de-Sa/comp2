41b5e8c57c9416228866d2022f5632e01323fdae
Marco-de-Sá <marcojb.desa@gmail.com>
2025-04-24 10:32:54 +0100

    added a comment that distinguishes WIP features

--- poker/pokerProject.py
+++ poker/pokerProject.py
@@ -2,9 +2,9 @@ from random import randint
 
 class GameDeck:
     def __init__(self, deck = []):
-        spCard = {1: "a", 2: "j", 3: "q", 4: "k"}
+        spCard = {1: "ace", 2: "jack", 3: "queen", 4: "king"}
         suit = {1: "clubs", 2: "spades", 3: "hearts", 4: "diamonds"}
-        deck = [f"a {suit[i]}" for i in range(1, 5)]
+        deck = [f"ace {suit[i]}" for i in range(1, 5)]
         for i in range(2, 11):
             for j in range(1, 5):
                 deck.append(f"{i} {suit[j]}")
@@ -31,6 +31,7 @@ class GameDeck:
         return temp
 
     def sort_deck(self):
+        # wip do not use
         cc = 0
         cs = 0
         for i in range(1, len(self.deck)):
@@ -54,17 +55,17 @@ class GameDeck:
             self.deck[ind2] = temp
 
     def to_string(self):
-        print(self.deck)
+        return f"{self.deck}"
 
 my_hand = GameDeck()
 my_hand.count_suit("hearts")
-my_hand.to_string()
+print(my_hand.to_string())
 my_hand.shuffle()
-my_hand.to_string()
+print(my_hand.to_string())
 tem = my_hand.deal_cards(4)
 print(tem)
-my_hand.to_string()
+print(my_hand.to_string())
 my_hand.rejoin(tem)
-my_hand.to_string()
-my_hand.sort_deck()
-my_hand.to_string()
+print(my_hand.to_string())
+# my_hand.sort_deck()
+# print(my_hand.to_string())
